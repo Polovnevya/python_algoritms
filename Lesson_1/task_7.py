273877a8807c9d60a27c68f82a21ff2934e36d05
Yuriy <PolovnevYA@gmail.com>
2021-01-20 23:41:38 +0300

    DONE

--- Lesson_1/task_7.py
+++ Lesson_1/task_7.py
@@ -1,6 +1,7 @@
 """
 По длинам трех отрезков, введенных пользователем, определить возможность существования треугольника,
     составленного из этих отрезков
+https://drive.google.com/file/d/1Ui2yipbPwl4C9ZbY4KEk6VSmB2YBMh-k/view?usp=sharing
 """
 segment_1 = float(input('Введите длинну первого отрезка '))
 segment_2 = float(input('Введите длинну второго отрезка '))
@@ -10,7 +11,7 @@ if segment_1 + segment_2 <= segment_3 or segment_2 + segment_3 <= segment_1 or s
     print(f'\nТакого треугольника не существует')
 elif segment_1 != segment_2 and segment_1 != segment_3 and segment_2 != segment_3:
     print(f'\nРазносторонний треугольник')
-elif segment_1 != segment_2 != segment_3:
+elif segment_1 == segment_2 == segment_3:
     print(f'\nРавносторонний треугольник')
 else:
     print(f'\nРавнобедренный треугольник')

70634f36d13eb532f85435f478074a92381428fa
Yuriy <PolovnevYA@gmail.com>
2021-01-20 22:48:21 +0300

    DONE

--- Lesson_1/task_1.py
+++ Lesson_1/task_1.py
@@ -1,5 +1,7 @@
 """
 Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+Блок-схема
+https://drive.google.com/file/d/1Ui2yipbPwl4C9ZbY4KEk6VSmB2YBMh-k/view?usp=sharing
 """
 
 digit = int(input('Введите целое число от 100 до 999 '))

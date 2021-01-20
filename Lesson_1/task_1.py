fcfdfd6bac9ccdb36caefb2f6ce7551c5b4b71a6
Yuriy <PolovnevYA@gmail.com>
2021-01-20 22:39:20 +0300

    almost done

--- Lesson_1/task_1.py
+++ Lesson_1/task_1.py
@@ -2,7 +2,7 @@
 Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
 """
 
-digit = int(input('Введите целое трехзначное число'))
+digit = int(input('Введите целое число от 100 до 999 '))
 
 first_digit = digit // 100
 second_digit = digit // 10 % 10
@@ -11,5 +11,5 @@ third_digit = digit % 10
 sum_digits = first_digit + second_digit + third_digit
 multiply_digits = first_digit * second_digit * third_digit
 
-print(f'Сумма цифр числа {digit} = {sum_digits}')
+print(f'\nСумма цифр числа {digit} = {sum_digits}')
 print(f'Произведение цифр числа {digit} = {multiply_digits}')

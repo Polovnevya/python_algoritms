165e9cf2d533a922e1f367a886f39644edcecc1a
Yuriy <PolovnevYA@gmail.com>
2021-01-21 00:14:55 +0300

    almost done

--- Lesson_1/task_9.py
+++ Lesson_1/task_9.py
@@ -2,8 +2,13 @@
 Вводятся три разных числа. Найти, какое из них является средним (больше одного, но меньше другого).
 """
 
-digit_1 = float(input('Введите длинну первого отрезка '))
-digit_2 = float(input('Введите длинну второго отрезка '))
-digit_3 = float(input('Введите длинну третьего отрезка '))
+digit_1 = float(input('Введите первое число '))
+digit_2 = float(input('Введите второе число '))
+digit_3 = float(input('Введите третье число '))
 
-
+if digit_2 <= digit_1 <= digit_3 or digit_2 >= digit_1 >= digit_3:
+    print(f'Число {digit_1} является средним')
+elif digit_1 <= digit_2 <= digit_3 or digit_1 >= digit_2 >= digit_3:
+    print(f'Число {digit_2} является средним')
+else:
+    print(f'Число {digit_3} является средним')

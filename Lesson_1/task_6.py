ca48f9368f9244115236aa728eda00b9fb0a4b86
Yuriy <PolovnevYA@gmail.com>
2021-01-21 22:14:33 +0300

    DONE!

--- Lesson_1/task_6.py
+++ Lesson_1/task_6.py
@@ -1,7 +1,6 @@
 """
 Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
 https://drive.google.com/file/d/1Ui2yipbPwl4C9ZbY4KEk6VSmB2YBMh-k/view?usp=sharing
-
 """
 
 symbol_num = int(input('Введите номер буквы в алфавите a-z '))

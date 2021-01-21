a6b8669d6c7d8740a6b126886a9a7fef208686c4
Yuriy <PolovnevYA@gmail.com>
2021-01-21 22:11:06 +0300

    DONE!

--- Lesson_1/task_6.py
+++ Lesson_1/task_6.py
@@ -1,6 +1,8 @@
 """
 Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+https://drive.google.com/file/d/1Ui2yipbPwl4C9ZbY4KEk6VSmB2YBMh-k/view?usp=sharing
+
 """
-# a = 97
+
 symbol_num = int(input('Введите номер буквы в алфавите a-z '))
 print(f'Буква с кодом {symbol_num}  это {chr(symbol_num)}')

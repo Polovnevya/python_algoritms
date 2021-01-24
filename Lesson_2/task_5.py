8a191b676390d3724e5cc2044c2972355b8a8711
Yuriy <PolovnevYA@gmail.com>
2021-01-24 20:53:24 +0300

    almost done

--- Lesson_2/task_5.py
+++ Lesson_2/task_5.py
@@ -5,17 +5,23 @@
 
 
 def print_pair(char_code: int) -> None:
-    print(f'"{chr(char_code)}" - {char_code}')
+    print(f'"{chr(char_code)}" - {char_code}', end='   ')
     return None
 
 
 def print_row(pair_number: int, char_code: int) -> None:
     print_pair(char_code)
-    if pair_number != 0:
+    if pair_number != 1:
         print_row(pair_number - 1, char_code + 1)
         return None
+    else:
+        print('')
+
+
+def print_table(char_code, row_num):
+    print_row(10, char_code)
 
 
 start = 32
-
-print_row(10, start)
+row_num = (127 - 32) / 10
+print_row(10, 32)

17198a1c187702d6b444fe9a0749b3bf57f130d3
Yuriy <PolovnevYA@gmail.com>
2021-01-24 21:35:30 +0300

    almost done

--- Lesson_2/task_5.py
+++ Lesson_2/task_5.py
@@ -5,11 +5,22 @@
 
 
 def print_pair(char_code: int) -> None:
+    """
+    производит вывод на печать пары - "символ" - Код символа
+    :param char_code: принимает код символа
+    :return: ничего не возвращает
+    """
     print(f'"{chr(char_code)}" - {char_code:^4}  ', end='   ')
     return None
 
 
 def print_row(pair_number: int, char_code: int) -> None:
+    """
+    производит печать строки таблицы, для печати пары Символ\код вызывает функцию print_pair
+    :param pair_number: количество пар в строке
+    :param char_code: код символа с которого надо начинать печать строки
+    :return: ничего не возвращает
+    """
     if char_code >= 32 and char_code <= 127:
         print_pair(char_code)
         if pair_number != 1:
@@ -21,14 +32,26 @@ def print_row(pair_number: int, char_code: int) -> None:
         return None
 
 
-def print_table(char_code: int, row_num, char_in_row):
-    print_row(char_in_row, char_code)
+def print_table(char_code: int, row_num: int, pair_in_row: int) -> None:
+    """
+    печатает таблицу с парами Символ\код. для печати строки вызывает функцию print_row
+    :param char_code: Код символа с которого надо начинать печатать таблицу
+    :param row_num: количество строк в таблице
+    :param char_in_row: символов в строке
+    :return:
+    """
+    print_row(pair_in_row, char_code)
     if row_num != 0:
-        print_table(char_code + char_in_row, row_num - 1, char_in_row)
+        print_table(char_code + pair_in_row, row_num - 1, pair_in_row)
 
 
+# Первый символ
 first_char_code = 32
+# последний символ
 last_char_code = 127
-char_in_row = 10
-row_num = round((last_char_code - first_char_code) / char_in_row)
-print_table(first_char_code, row_num, char_in_row)
+# пар в строке
+pair_in_row = 10
+# количество строк
+row_num = round((last_char_code - first_char_code) / pair_in_row)
+
+print_table(first_char_code, row_num, pair_in_row)

076d8e3a96382f11df2ac8873d2fcc383ff2535b
Yuriy <PolovnevYA@gmail.com>
2021-01-24 22:32:25 +0300

    almost done

--- Lesson_2/task_6.py
+++ Lesson_2/task_6.py
@@ -8,22 +8,28 @@
 
 from random import randint
 
-attempt_param = 10
-secret_num = randint(0, 101)
-
 
 def check_user_num(user_num: int, secret_num: int) -> str:
     if secret_num > user_num:
-        return f'+'
+        return f'Ваше число меньше'
     elif secret_num < user_num:
-        return f'_'
+        return f'Ваше число больше'
     else:
-        return f'='
+        return f'Победа'
 
 
 def guess_num(attempt: int, num: int):
     user_num = int(input(f'Угадайте число от 0 до 100, у вас {attempt} попыток '))
     if attempt > 0:
-        print (check_user_num(user_num, num))
+        if check_user_num(user_num, num) != 'Победа':
+            print(check_user_num(user_num, num))
+            guess_num(attempt-1, num)
+        else:
+            print(f'Вы победили! Было загадано число {num}')
     else:
         print(f'Вы проиграли, было загадано число {num}')
+
+
+attempt_param = 10
+secret_num = randint(0, 100)
+a = guess_num(attempt_param, secret_num)

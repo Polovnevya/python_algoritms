337c9a5f5eda81213489aefe7e5c655088df7738
Yuriy <PolovnevYA@gmail.com>
2021-01-27 20:29:17 +0300

    DONE

--- Lesson_3/task_2.py
+++ Lesson_3/task_2.py
@@ -4,3 +4,13 @@
     второй массив надо заполнить значениями 0, 3, 4, 5, (индексация начинается с нуля),
     т.к. именно в этих позициях первого массива стоят четные числа.
 """
+
+from random import randint
+
+MIN_ITEM = 0
+MAX_ITEM = 100
+SIZE = 10
+array = [randint(MIN_ITEM, MAX_ITEM) for i in range(SIZE)]
+print(array)
+second_array = [index for index, val in enumerate(array) if val % 2 == 0]
+print(second_array)

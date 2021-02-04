4e2d1ae8caf3dcd98222caaf8b17651d7dc0a486
Yuriy <PolovnevYA@gmail.com>
2021-02-04 18:42:46 +0300

    get started

--- Lesson_5/task_2.py
+++ Lesson_5/task_2.py
@@ -5,13 +5,13 @@
 Нужно сохранить их как [‘A’, ‘2’] и [‘C’, ‘4’, ‘F’] соответственно.
 Сумма чисел из примера: [‘C’, ‘F’, ‘1’], произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
 from collections import deque
 
 
 def hex_sum():
     hex_num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-    first_num = deque(input('Введите первое 16ричное число: '))
-    second_num = deque(input('Введите второе 16ричное число: '))
+
 
     if first_num < second_num:
         first_num, second_num = second_num, first_num
@@ -22,20 +22,21 @@ def hex_sum():
         if second_num:
             ham = second_num.pop()
             egg_index = (hex_num.index(spam) + hex_num.index(ham) + k) % 16
-            egg = hex_num[tmp]
-            third.append(egg)
+            egg = hex_num[egg_index]
+            third.appendleft(egg)
         else:
             egg_index = (hex_num.index(spam) + k) % 16
-            egg = hex_num[tmp]
-            third.append(egg)
+            egg = hex_num[egg_index]
+            third.appendleft(egg)
 
         if (hex_num.index(spam) + hex_num.index(ham)) >= 15:
             k = 1
         else:
             k = 0
+    return third
 
-    return third.reverse()
 
-
+first_num = deque(input('Введите первое 16ричное число: '))
+second_num = deque(input('Введите второе 16ричное число: '))
 a = hex_sum()
 b = 1
